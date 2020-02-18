a3b45282623f9db27a7e624fa91042db43e79ee9
Raghav <raghav.chopra@zenatix.com>
2020-02-18 09:39:16 +0530

    Added Product Snippets

--- ecommerce/views.py
+++ ecommerce/views.py
@@ -29,7 +29,8 @@ def contact_page(request):
     context = {
         "title": "Contact Us",
         "content": "Welcome to Contact Page",
-        "form": contact_form
+        "form": contact_form,
+        "brand": "new Brand Name"
     }
     if contact_form.is_valid():
         print(contact_form.cleaned_data)
